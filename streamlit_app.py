270d9d089f8a0c767244d9554f19db99cdaa3b46
Ashwithagattu <87004427+Ashwithagattu@users.noreply.github.com>
2023-08-18 13:24:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,13 @@ streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-selected_fruits = streamlit.multiselect("Pick some fruits:", options=list(my_fruit_list.index), default=["Avocado", "Strawberries"])
-fruits_to_show = my_fruit_list.loc[selected_fruits]
-streamlit.dataframe(fruits_to_show)
+
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
+import pandas
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
